d9c2725f71959287a8845cd1e86979d0c066ce9c
mtyuksel <yukselmtevfik@gmail.com>
2021-05-28 22:17:28 +0300

    Folder structure has changed.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { FooterComponent } from "./footer/footer.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
+import { FooterComponent } from "./master_components/footer/footer.component";
+import { NavbarComponent } from "./master_components/navbar/navbar.component";
+import { SidebarComponent } from "./master_components/sidebar/sidebar.component";
 
 @NgModule({
   imports: [CommonModule, RouterModule, NgbModule],
