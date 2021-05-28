aa8c2574ca26d3aa840a74435e92e92c9a6542a7
mtyuksel <yukselmtevfik@gmail.com>
2021-05-28 21:38:22 +0300

    Cars component works with notification service.

--- src/app/pages/cars/cars.component.ts
+++ src/app/pages/cars/cars.component.ts
@@ -1,6 +1,6 @@
+import { SummaryResolver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { Car } from 'src/app/models/car';
+import { MatDialog } from '@angular/material/dialog';
 import { CarDetailDto } from 'src/app/models/DTOs/carDetailDto';
 import { CarService } from 'src/app/services/car.service';
 import { NotificationService } from 'src/app/services/notification.service';
@@ -15,8 +15,8 @@ export class CarsComponent implements OnInit {
   carDetails: CarDetailDto[] = [];
 
   constructor(private carService: CarService, 
-    private toastrService: ToastrService, 
-    private notificationService: NotificationService) { }
+    private notificationService: NotificationService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getCarDetails();
@@ -39,14 +39,17 @@ export class CarsComponent implements OnInit {
   }
 
   removeCar(carDetail: CarDetailDto) {
+    // const dialogRef = this.dialog.open(are-you-SummaryResolver)
+    // this.dialog.openDialogs()
+
     if (confirm("Are you use you want to delete this car: " + carDetail.id + " / " + carDetail.brand.name + " / " + carDetail.name)) {
       this.carService.deleteCarWithCarDetailDto(carDetail).subscribe(rp => {
-        this.toastrService.info("Succesfully deleted!");
+        this.notificationService.showInfo("Succesfully deleted!");
         console.log("Deleted!", rp);
       })
     }
     else {
-      this.toastrService.info("Car not deleted.");
+      this.notificationService.showInfo("Car not deleted.");
     }
   }
 }
